dcac983f23a144b94288265c7400dcc4d5fd6a22
Harminder virk <virk.officials@gmail.com>
2019-05-14 10:53:50 +0530

    feat: add response class to sending response

---
+++ index.ts
@@ -0,0 +1,18 @@
+/*
+* @poppinss/response
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+/// <reference path="./src/contracts.ts" />
+
+export {
+  ResponseContract,
+  ResponseConfigContract,
+  ResponseConstructorContract,
+} from '@poppinss/response/contracts'
+
+export { Response } from './src/Response'
---
+++ src/contracts.ts
@@ -0,0 +1,116 @@
+/*
+ * @poppinss/response
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare module '@poppinss/response/contracts' {
+  import { MacroableConstructorContract } from 'macroable'
+  import { ServerResponse, IncomingMessage } from 'http'
+  import { CookieOptions } from '@adonisjs/cookie'
+
+  /**
+   * Types from which response header can be casted to a
+   * string
+   */
+  type CastableHeader = string | number | boolean | string[] | number[] | boolean[]
+
+  /**
+   * Content types after processing response body
+   */
+  type ResponseContentType =
+    'text/html' |
+    'text/plain' |
+    'application/octet-stream' |
+    'application/json' |
+    'unknown' |
+    'null'
+
+  /**
+   * Types of readable stream allowed for HTTP response
+   */
+  type ResponseStream = NodeJS.ReadStream | NodeJS.ReadWriteStream | NodeJS.ReadableStream
+
+  /**
+   * Lazy body packet
+   */
+  type LazyBody = {
+    writer: any,
+    args: any[],
+  }
+
+  /**
+   * Config accepted by response class
+   */
+  type ResponseConfigContract = {
+    etag: boolean,
+    jsonpCallbackName: string,
+    secret: string,
+    cookie: Partial<CookieOptions>,
+  }
+
+  /**
+   * Main response interface
+   */
+  interface ResponseContract {
+    explicitEnd: boolean
+    hasLazyBody: boolean
+    lazyBody: LazyBody | null
+    finished: boolean
+    headersSent: boolean
+    isPending: boolean
+    request: IncomingMessage
+    response: ServerResponse
+
+    getHeader (key: string): string | string[] | number | undefined
+    header (key: string, value: CastableHeader): this
+    append (key: string, value: CastableHeader): this
+    safeHeader (key: string, value: CastableHeader): this
+    removeHeader (key: string): this
+
+    status (code: number): this
+    safeStatus (code: number): this
+    type (type: string, charset?: string): this
+    vary (field: string): this
+    setEtag (body: any, weak?: boolean): this
+
+    buildResponseBody (body: any): { body: any, type: ResponseContentType, originalType?: string }
+    send (body: any, generateEtag?: boolean): void
+    json (body: any, generateEtag?: boolean): void
+    jsonp (body: any, callbackName?: string, generateEtag?: boolean): void
+
+    stream (stream: ResponseStream, raiseErrors?: boolean): Promise<Error | void>
+    download (filePath: string, generateEtag?: boolean, raiseErrors?: boolean): Promise<Error | void>
+    attachment (
+      filePath: string,
+      name?: string,
+      disposition?: string,
+      generateEtag?: boolean,
+      raiseErrors?: boolean,
+    ): Promise<Error | void>
+
+    location (url: string): this
+    redirect (url: string, reflectQueryParams?: boolean, statusCode?: number): void
+
+    cookie (key: string, value: any, options?: CookieOptions): this
+    plainCookie (key: string, value: any, options?: CookieOptions): this
+    clearCookie (key: string): this
+
+    finish (): void
+  }
+
+  /**
+   * Constructor contract is required coz of static methods
+   * on the response class
+   */
+  interface ResponseConstructorContract extends MacroableConstructorContract {
+    new (
+      request: IncomingMessage,
+      response: ServerResponse,
+      config: Partial<ResponseConfigContract>,
+    ): ResponseContract
+  }
+}
